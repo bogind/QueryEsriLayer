39a1ef3ea043c4c0f30670364fab741c4bc2c2e0
bogind <dror@kaplanopensource.co.il>
2020-11-23 22:15:56 +0200

    basic version with querying feature layers

--- map.js
+++ map.js
@@ -1,12 +1,91 @@
-var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-var map = L.map('map',{
+
+let map = L.map('map',{
 	layers: OpenStreetMap_Mapnik,
-	center: [0.0, 0.0],
+	center: [32.69486, 12.6123],
 	zoom: 4,
 	maxZoom: 18,
     minZoom: 0,
 })
+
+L.Control.Menu =  L.Control.extend({
+    onAdd: function(map) {
+		var container = L.DomUtil.create('div','menu leaflet-bar');
+		var span = L.DomUtil.create('span');
+		var urlInput = L.DomUtil.create('input', 'url-input');
+		var loadButton = L.DomUtil.create('input', 'load-button');
+
+		urlInput.id = "urlInput";
+		urlInput.type = "url";
+		loadButton.id = "load";
+		loadButton.type = "button"
+		loadButton.value = "Add"
+		loadButton.onclick = function(){
+			var url = document.getElementById("urlInput").value
+			lastPart = url.slice(url.lastIndexOf("/")+1)
+			if(!Number.isNaN(parseInt(lastPart)) && Number.isInteger(parseInt(lastPart))){
+				getLayerDetails(url+"?f=json")
+					.then(data =>
+						layerMetadata = data
+						)
+					.then(
+						layerMetadata =>
+						chooseDownload(layerMetadata,url)
+					)
+					.then(
+
+					)
+					
+			}
+			
+		}
+		
+
+		span.append(urlInput, loadButton)
+		container.append(span)
+
+		return container
+	},
+
+	onRemove: function(map) {
+        // Nothing to do here
+	}
+})
+L.control.menu = function(opts) {
+    return new L.Control.Menu(opts);
+}
+
+let menu =  L.control.menu({ position: 'topleft' })
+
+L.Control.OpenInput =  L.Control.extend({
+    onAdd: function(map) {
+        var container = L.DomUtil.create('div','button leaflet-bar');
+
+		container.style.color = 'green';
+		container.innerText = '+';
+
+
+		container.onclick = function(){
+			if(menu._map){
+				map.removeControl(menu)
+			}else{
+				menu.addTo(map)
+			}
+		}
+
+        return container;
+    },
+
+    onRemove: function(map) {
+        // Nothing to do here
+    }
+});
+L.control.openInput = function(opts) {
+    return new L.Control.OpenInput(opts);
+}
+
+let openInputButton =  L.control.openInput({ position: 'topleft' }).addTo(map);
---
+++ utils.js
@@ -0,0 +1,61 @@
+async function getLayerDetails(url){
+    const response  = await fetch(url);
+
+    return response.json();
+}
+let a2g = ArcgisToGeojsonUtils;
+function chooseDownload(layerMetadata,url){
+    formats = layerMetadata.supportedQueryFormats.split(", ")
+    fields = layerMetadata.fields
+    for(var i=0;i<fields.length;i++){
+        field = fields[i]
+        if(field.type == "esriFieldTypeOID"){
+            var OBJECTID = field.name
+        }
+    }
+    if(formats.includes("geoJSON")){
+        layerType = "GeoJSON"
+        /*
+        TODO: use returnIdsOnly=true&f=pjson to query only IDS and get layer length, 
+              creating multiple requests and streaming more data to layer after loading.
+        */
+        queryUrl = url+`/query?where=${OBJECTID} > 0&spatialRel=esriSpatialRelIntersects&outFields=*&returnGeometry=true&resultRecordCount=4000&geometryPrecision=6&outSR=4326&f=geojson`
+    }else if(!formats.includes("geoJSON") && formats.includes("JSON")){
+        layerType = "JSON"
+        queryUrl = url+`/query?where=${OBJECTID} > 0&spatialRel=esriSpatialRelIntersects&outFields=*&returnGeometry=true&resultRecordCount=4000&geometryPrecision=6&outSR=4326&f=pjson`
+    }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            
+            if(layerType == "JSON"){
+                gj = a2g.arcgisToGeoJSON(JSON.parse(xhttp.responseText))
+            }else{
+                gj = JSON.parse(xhttp.responseText)
+            }
+            L.geoJson(gj,{
+                onEachFeature: function onEachFeature(feature, layer) {
+                    if (feature.properties){
+                    popupContent = "<table style='width:100%'>"
+                    for(var i=0;i<fields.length;i++){
+                        field = fields[i]
+                        if(field.type != "esriFieldTypeOID" && field.type != "esriFieldTypeGeometry" ){
+                            if(field.alias){
+                                popupContent += `<tr><td><b>${field.alias}</b></td><td>${feature.properties[field.name]}</td></tr>`
+                            }
+                        }
+                    }
+                    popupContent += "</table>"
+                    
+                    layer.bindPopup(popupContent);
+                    }
+                }
+            })
+            .addTo(map)
+            //return JSON.parse(xhttp.responseText);
+        }
+    };
+    xhttp.open("GET", queryUrl, true);
+    xhttp.send();
+
+}
